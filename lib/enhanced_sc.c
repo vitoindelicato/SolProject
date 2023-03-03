2cc659ffc116607f142d18e7b3c61ac0388d294e
vitoindelicato <indelicato.vito99@gmail.com>
2023-03-03 10:51:14 +0100

    writing system calls with error handling, it could need some modifications later...

---
+++ lib/enhanced_sc.c
@@ -0,0 +1,75 @@
+#include "enhanced_sc.h"
+#include <stdlib.h>
+
+
+/* THREAD FUNCTIONS SECTION */
+int lock(pthread_mutex_t *mutex){
+    int err;
+    if((err = pthread_mutex_lock(mutex)) != 0){
+        perror("Error while locking mutex");
+        exit(EXIT_FAILURE);
+    }
+}
+
+int unlock(pthread_mutex_t *mutex){
+    int err;
+    if((err = pthread_mutex_unlock(mutex)) != 0){
+        perror("Error while unlocking mutex");
+        exit(EXIT_FAILURE);
+    }
+}
+
+
+int cancel(pthread_t *tid){
+    int err;
+    if((err = pthread_cancel(*tid)) != 0){
+        perror("Error while canceling thread");
+        exit(EXIT_FAILURE);
+    }
+}
+
+
+int create(pthread_t *tid, const pthread_attr_t *attr, void* (*function) (void *), void *args){
+    int err;
+    if((err = pthread_create(tid, attr, function, args)) != 0){
+        perror("Error while creating thread");
+        exit(EXIT_FAILURE);
+    }
+}
+
+int join(pthread_t thread_id, void **retval){
+    int err;
+    if((err = pthread_join(thread_id, retval)) != 0){
+        perror("Error while joining thread");
+        exit(EXIT_FAILURE);
+    }
+}
+
+
+int cond_wait(pthread_cond_t *cond, pthread_mutex_t *mutex){
+    int err;
+    if((err = pthread_cond_wait(cond, mutex)) != 0){
+        perror("Error while waiting for condition");
+        exit(EXIT_FAILURE);
+    }
+}
+
+
+
+int cond_signal(pthread_cond_t *cond){
+    int err;
+    if((err = pthread_cond_signal(cond)) != 0){
+        perror("Error while signaling condition");
+        exit(EXIT_FAILURE);
+    }
+}
+
+
+int cond_init (pthread_cond_t * cnd, const pthread_condattr_t * attr){
+    int err;
+    if((err = pthread_cond_init(cnd, attr)) != 0){
+        perror("Error while initializing condition");
+        exit(EXIT_FAILURE);
+    }
+}
+
--- lib/enhanced_sc.h
+++ lib/enhanced_sc.h
@@ -1,17 +1,26 @@
 #include <pthread.h>
 
+
+/* THREADS SYSTEM CALLS */
 int lock(pthread_mutex_t *mutex);
 int unlock(pthread_mutex_t *mutex);
 int cancel(pthread_t *tid);
 
-
-int create(pthread_t *thread_id, const pthread_attr_t *attr, void* (*function) (void *), void *args);
+int create(pthread_t *tid, const pthread_attr_t *attr, void* (*function) (void *), void *args);
 int join(pthread_t thread_id, void **retval);
 
 int cond_wait(pthread_cond_t *cond, pthread_mutex_t *mutex);
 int cond_signal(pthread_cond_t *cond);
+int cond_init (pthread_cond_t * cnd, const pthread_condattr_t * attr);
+
+/* MEMORY ALLOCATION SYSTEM CALLS */
+void *Malloc(size_t size);
 
 
+
+/*   READ AND WRITES   */
+ssize_t readn(int fd, void *ptr, size_t n);
+ssize_t writen(int fd, void *ptr, size_t n);
 
 
 
